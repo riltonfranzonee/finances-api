893ae3a6b2e0ba22f3c4df31a8f7dcfba4fdf0b6
Rilton Franzone <riltonfranzone@gmail.com>
2020-05-09 18:26:32 -0300

    [feat] list transactions and create (handle with category to do)

---
+++ src/routes/transactions.routes.ts
@@ -0,0 +1,43 @@
+import { Router } from 'express';
+
+import { getCustomRepository } from 'typeorm';
+
+import TransactionsRepository from '../repositories/TransactionsRepository';
+import CreateTransactionService from '../services/CreateTransactionService';
+// import DeleteTransactionService from '../services/DeleteTransactionService';
+// import ImportTransactionsService from '../services/ImportTransactionsService';
+
+const transactionsRouter = Router();
+
+transactionsRouter.get('/', async (request, response) => {
+  const transactionsRepository = getCustomRepository(TransactionsRepository);
+
+  const transactions = await transactionsRepository.find();
+
+  return response.json(transactions);
+});
+
+transactionsRouter.post('/', async (request, response) => {
+  const { title, value, type, category } = request.body;
+
+  const createTransaction = new CreateTransactionService();
+
+  const transaction = await createTransaction.execute({
+    title,
+    value,
+    type,
+    category,
+  });
+
+  return response.json(transaction);
+});
+
+transactionsRouter.delete('/:id', async (request, response) => {
+  // TODO
+});
+
+transactionsRouter.post('/import', async (request, response) => {
+  // TODO
+});
+
+export default transactionsRouter;
